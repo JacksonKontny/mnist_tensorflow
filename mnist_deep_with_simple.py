862caeb8e8fac02d6ce9b3d092b8186e0a04bcac
Jackson <jackson.kontny@analytehealth.com>
2017-10-28 20:19:35 -0500

    remove main from simple

--- mnist_deep_with_simple.py
+++ mnist_deep_with_simple.py
@@ -183,108 +183,10 @@ def create_variable_summaries(var_list):
       tf.summary.scalar('min', tf.reduce_min(var))
       tf.summary.histogram('histogram', var)
 
-def main(_, *args, **kwargs):
-  # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-
-  # Create the model
-  # create an input variable for x, our image.  The number of images in the
-  # input can be of any size
-  x = tf.placeholder(tf.float32, [None, 784])
-
-  # Define loss and optimizer
-  # create an input variable for the labeled dataset.  There can be any number
-  # of labels based on the input size, but they must have an output vector of
-  # 10 for each digit
-  y_ = tf.placeholder(tf.float32, [None, 10])
-
-  # Build the graph for the deep net
-  y_conv, keep_prob = deepnn(x)
-
-  # set this part of the graph to 'loss' because we are computing the loss
-  # function here
-  with tf.name_scope('loss'):
-    with tf.name_scope('cross_entropy'):
-      # Use tf's magic cross entropy function to compute the ce loss between the
-      # predicted digits and the labeled digits
-      cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_,
-                                                              logits=y_conv)
-    with tf.name_scope('cross_entropy_mean'):
-      # get the average cross entropy loss in this layer accross the input examples
-      cross_entropy = tf.reduce_mean(cross_entropy)
-  tf.summary.scaler('cross_entropy', cross_entropy)
-
-  # use the adam optimizer to train our neural network, since it is better and
-  # faster than regular backpropagation
-  with tf.name_scope('adam_optimizer'):
-    # our goal is to minimize the cross entropy
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-  # Name this part of the graph accuracy - we will be calculating our accuracy
-  # on the training set here
-  with tf.name_scope('accuracy'):
-    # we will know if our prediction is correct if the highest output vector on
-    # the final layer is the same as what the label set suggests
-    with tf.name_scope('correct'):
-      correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-
-      # We will cast our prediction to a float from a boolean so we can compute
-      # the accuracy
-      correct_prediction = tf.cast(correct_prediction, tf.float32)
-    with tf.name_scope('accuracy'):
-
-      # The accuracy is just the total predicted correct / the size of the dataset.
-      # Since the correct prediction vector is 0's and 1's this will perform that
-      # calculation
-      accuracy = tf.reduce_mean(correct_prediction)
-  tf.summary.scaler('accuracy', accuracy)
-
-  # create a location to create and store the graph info
-  merged = tf.summary.merge_all()
-
-  # Merge all summary info here, so we can write it out.
-  train_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/tmp/tf/train',
-                                      sess.graph)
-  test_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/tmp/tf/test')
-  tf.summary.merge_all()
-
-  # initialize the default graph info on the summary writer object
-  train_writer.add_graph(tf.get_default_graph())
-  test_writer.add_graph(tf.get_default_graph())
-
-  # start the session - now the actual computation happens
-  with tf.Session() as sess:
-    # initialize all of the random variables
-    sess.run(tf.initialize_all_variables())
-
-    # loop over the epocs
-    for i in range(10000):
-      # get the next 50 examples for the batch
-      batch = mnist.train.next_batch(50)
-
-      # gather performance metrics every 100 epocs
-      if i % 100 == 0:
-        # evaluate the training set accuracy.  We use a keep_prob of 1 because
-        # we don't want to drop any of the neurons during evaluation
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        # output the training accuracy
-        print('step %d, training accuracy %g' % (i, train_accuracy))
-      # run feedforward and backpropagation on the batch.  We use a keep
-      # probability of .5, meaning we remove a neurons influence on the output
-      # randomly half the time
-      train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-    # Once the training has completed, lets compute the test accuracy, using
-    # the test dataset
-    print('test accuracy %g' % accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--data_dir', type=str,
                       default='/tmp/tensorflow/mnist/input_data',
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main)# , argv=[sys.argv[0]] + unparsed)
-  # tf.app.run(main=simple_main)# , argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=simple_main)# , argv=[sys.argv[0]] + unparsed)
